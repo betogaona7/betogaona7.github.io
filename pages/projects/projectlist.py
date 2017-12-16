8ebc2b69ec3938aa2a1e270492be9247afe0eae5
Alberto Gaona <albertoo_3c@hotmail.com>
2017-12-16 17:14:50 -0600

    add complete description of Mariya

--- pages/projects/projectlist.py
+++ pages/projects/projectlist.py
@@ -117,9 +117,76 @@ OWN_project1 = media.Project("Mariya",
 
 						  "https://media.giphy.com/media/TGHMbONT5eGgU/giphy.gif",
 						  "https://player.vimeo.com/video/195152250",
-						  "Technical description not available yet -",
-						  "https://github.com/proyectomariya/translate")
 
+						  """<div style='text-align: center;'>
+						  		<video controls poster='./img/huicholdos.png'>
+                                <source src='./img/Mariya.mp4' type='video/mp4'>
+                                </video>
+                          	 </div><br/><br/><p>
+
+						  A Mexican project that opens a communication portal between an indigenous community and the rest of the world. The Wixarikas
+						  erroneously known as Huicholes are part of a Mexican town. Globally known for their art: The Huichol art.<br/> <br/>
+
+						  Do you know that every piece of art they create really tells a story? each bracelet, each table, each figure tells something 
+						  about their culture. Those stories create an identity for them. Through their tales, which have passed from generation to 
+						  generation, they create a heritage to encourage new generations to follow in the footsteps of their ancestors.<br/> <br/>
+
+						  Apart from facing the possible disapperence of their sacred land due mining, day by day they face a problem that goes unnoticed:
+						  'migration'. Being located in the highest part of the Sierra Madre Occidental, communication and access to new technologies is 
+						  almost impossible. This force the new generations to migrate to the big cities, confronting the language problem that threatens 
+						  the extinction of their native language.<br/> <br/>
+
+						  The Mariya project tries to demonstrate that the technology is not fought with the ancestral customs of a town, giving the floor
+						  to those who deserve to be heard, seeking to rescue their stories and knowledge which they consider sacred.<br/> <br/>
+
+						  Fall in love with the Wixarika culture with the documentary: <a target='_blank' rel='noopener' href='https://huicholesfilm.com/en/'>
+						  Huicholes the last peyote guardians</a>.
+						  <br/>Learn more about the Mariya project by visiting the <a target='_blank' rel='noopener' href='https://proyectomariya.github.io/'>
+						  official website</a>.<br/><br/>""",
+						  
+						  "https://github.com/proyectomariya/translate")
+"""
+Un proyecto Mexicano que abre un portal de comunicacion entre una comunidad indigena y el resto del mundo. Los Wixaricas 
+erroneamente conocidos como Huicholes son parte de un pueblo Mexicano, mundialmente conocidos por su arte: el arte huichol.
+sabias tu que cada pieza de arte que ellos crean realmente cuenta una historia. cada pulsera, cada tabla, cada figura cuenta
+algo de su cultura.
+
+Esas historias les crean una identidad. A traves de sus cuentos, los cuales han pasado de generacion en generacion 
+crean una herencia para incentivar a las nuevas generaciones a seguir los pasos de sus antepasados. 
+
+Aparte de enfrentarse a la posible desaparecion de su tierra sagrada debido a la mineria canadiense, dia a dia se enfrentan a un
+problema que pasa desapercibido: "la migracion". Al estar asentados en lo mas alto de la Sierra madre occidental, la comunicacion
+y el acceso a las nuevas tecnologias les resulta casi imposible. Esto obliga a las nevas generaciones migrar a las grandes
+ciudades, enfrentandose con el problema del lenguaje que amenza con la extincion de su lengua materna. 
+
+Proyecto Mariya pretende demostrar que la tecnologia no esta peleada con las costrumbres ancestrales de un pueblo, dandole la 
+palabra a los que mrecen ser escuchados. buscando rescatar sus historias y conocimientos los cuales ellos consideran como sagrados.
+
+
+Enamorate de la cultura Wixarica con el documental: <a>
+Para conocer aun mas sobre el proyeto mariya visita la pagina
+
+[Fotos]
+
+Conoce mas sobre los huicholes con este interseante documental: <a href='https://huicholesfilm.com/en/'>Huicholes, the last peyote guardians </a>
+Visita la pagina del dispositivo: <a href='https:projectomariya.github.io'>Proyecto mariya</a>
+
+A Mexican project that opens a communication portal between an indigenous community and the rest of the world. The Wixarikas 
+erroneously known as Huicholes are part of a Mexican town. Globally known for their art: The Huichol art.
+
+Do you know that every piece of art they create really tells a story? each bracelet, each table, each figure tells something 
+about their culture. Those stories create an identity for them. Through their tales, which have passed from generation to 
+generation, they create a heritage to encourage new generations to follow in the footsteps of their ancestors.
+
+Apart from facing the possible disapperence of their sacred land due mining, day by day they face a problem that goes unnoticed:
+"migration". Being located in the highest part of the Sierra Madre Occidental, communication and access to new technologies is 
+almost impossible. This force the new generations to migrate to the big cities, confronting the language problem that threatens 
+the extinction of their mother tongue. 
+
+The Mariya project tries to demonstrate that the technology is not fought with the ancestral customs of a town, giving the floor
+to those who deserve to be heard, seeking to rescue their stories and knowledge which they consider sacred. 
+
+"""
 projectslist = [OWN_project1, DL_project1, AI_project4, AI_project3, DL_project3, DL_project5, AI_project2, AI_project1, DL_project2, DL_project4 ]
 projects.open_project_page(projectslist)
 
